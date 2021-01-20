92c8bf2b150671a6cc1527e323b10ccb39a0954a
kenny67 <282716545@qq.com>
2021-01-21 01:26:18 +0900

    init env2

---
+++ scripts/start_server.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+echo "start server"
+cd /var/www/actionview
+
+# sudo chown -R apache:apache /var/www/laravel1
+sudo chown -R ec2-user:ec2-user /var/www/actionview
+
+sudo chmod 775 /var/www/actionview/storage
+sudo chmod 775 /var/www/actionview/bootstrap/cache
+
+# 第一步  关闭docker 镜像
+cd /var/www/actionview
+/usr/local/bin/docker-compose build
+/usr/local/bin/docker-compose up -d
+# docker start laravel_cms_php_1
+
+# npm run dev
+# composer update
+# sudo service httpd restart
+#php artisan serve --port 80
--- scripts/stop_server.sh.tpl
+++ scripts/stop_server.sh.tpl
@@ -7,9 +7,9 @@ echo "stop server 停止服务"
 # 第一步  关闭docker 镜像
 #docker stop laravel_cms_php_1
 
-cd /var/www/actionview/
+cd /var/www/${PROJECT_NAME}/
 /usr/local/bin/docker-compose down
 
-#sudo rm -rf /var/www/actionview/*
+#sudo rm -rf /var/www/${PROJECT_NAME}/*
 # kill -s 9 8001
 
