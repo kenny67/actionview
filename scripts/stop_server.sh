3e0a8f772b48e2fb10e8ec617e915e2e66eaa628
kenny67 <282716545@qq.com>
2021-01-21 01:47:42 +0900

    init env10

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -5,10 +5,10 @@ echo "stop server 停止服务"
 
 echo "docker stop"
 # 第一步  关闭docker 镜像
-## docker stop laravel_cms_php_1
+
 
 cd /var/www/actionview/
-# /usr/local/bin/docker-compose down
+
 
 sudo rm -rf /var/www/actionview/*
 ## kill -s 9 8001
