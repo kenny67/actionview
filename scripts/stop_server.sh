a908c31248874fa20465688d82223e2cf7305a33
kenny67 <282716545@qq.com>
2021-01-21 01:43:36 +0900

    init env4

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -3,9 +3,9 @@
 echo "stop server 停止服务"
 #sudo service httpd stop
 
-
+echo "docker stop"
 # 第一步  关闭docker 镜像
-##docker stop laravel_cms_php_1
+## docker stop laravel_cms_php_1
 
 cd /var/www/actionview/
 # /usr/local/bin/docker-compose down
--- scripts/stop_server.sh.tpl
+++ scripts/stop_server.sh.tpl
@@ -3,7 +3,7 @@
 echo "stop server 停止服务"
 #sudo service httpd stop
 
-
+echo "docker stop"
 # 第一步  关闭docker 镜像
 ##docker stop laravel_cms_php_1
 
